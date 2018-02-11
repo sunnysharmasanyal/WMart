0bcbf15ac2058ebe0ec97c36742ecf5d9aa4e78d
SandipKumar068 <dev.sandipkumar@gmail.com>
2018-02-11 23:05:58 +0530

    commit

---
+++ WMart/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WMart.Models;
+
+namespace WMart.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning Sir" : "Good Afternoon Sir";
+            return View();
+        }
+        //Creating new ActionResult for URL 
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse wmart)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["MvcConn"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+
+
+                {
+                    string query = "INSERT INTO Attend(Name, CEmail, CPhone, Status) VALUES(@Name,@CEmail,@CPhone,@Status)";
+                    query += " SELECT SCOPE_IDENTITY()";
+                    using (SqlCommand cmd = new SqlCommand(query))
+                    {
+                        cmd.Connection = con;
+                        con.Open();
+                        cmd.Parameters.AddWithValue("@Name", wmart.Name);
+                        cmd.Parameters.AddWithValue("@CEmail", wmart.CEmail);
+                        cmd.Parameters.AddWithValue("@CPhone", wmart.CPhone);
+                        cmd.Parameters.AddWithValue("@Status", wmart.Status);
+
+                        wmart.CId = Convert.ToInt32(cmd.ExecuteScalar());
+                        con.Close();
+                    }
+                }
+            }
+
+            return View(wmart);
+        }
+
+    }
+}
---
+++ WMart/Models/GuestResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WMart.Models
+{
+    public class GuestResponse
+    {
+        
+        public int CId { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        public string CEmail { get; set; }
+
+        [Required(ErrorMessage = "Phone is required.")]
+        public string CPhone { get; set; }
+
+
+        public string Status { get; set; }
+    }
+}
