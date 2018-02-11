fdf8fb234afc66d72bb51132c9a1543de055feec
SunnySanyal <sunnysanyal@sam>
2018-02-12 01:34:40 +0530

    Extension Methods Uncomplete

---
+++ WMart/Models/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WMart.Models
+{
+    public class ShoppingCart
+    {
+        // Shopping Cart using Extension Methods
+        // Extension Methods are a convenent way of adding methods to class that you
+        // Dont own  and cannot modify directly 
+        public List<Products> Products { get; set; }
+    }
+}
